97edbf5e666680988f836ec3d46aa8503f3af813
Vera Krivenko <v7771@yandex.ru>
2020-07-13 22:02:24 -0500

    Initial commit

---
+++ src/test/java/ru/geekbrains/java/oop/at/participants/Runner.java
@@ -0,0 +1,17 @@
+package ru.geekbrains.java.oop.at.participants;
+
+public interface Runner {
+    String getName();
+
+    int getMaxLength();
+
+    int getMaxWeight();
+
+    boolean getSuccess();
+
+    void setSuccess(boolean success);
+
+    void run();
+
+    void jump();
+}
