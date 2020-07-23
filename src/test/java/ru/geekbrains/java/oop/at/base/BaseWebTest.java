8c159d511194e0229b8d2a6dec828d71ffa52782
Vera Krivenko <v7771@yandex.ru>
2020-07-23 00:56:40 -0500

    HW_4_

--- src/test/java/ru/geekbrains/java/oop/at/base/BaseWebTest.java
+++ src/test/java/ru/geekbrains/java/oop/at/base/BaseWebTest.java
@@ -2,6 +2,7 @@ package ru.geekbrains.java.oop.at.base;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -9,34 +10,34 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
-public abstract class BaseTest {
+public abstract class BaseWebTest {
 
-    public ChromeDriver chromeDriver;
+    public WebDriver driver;
     public WebDriverWait wait15second;
 
     @BeforeEach
     public void baseTestBeforeAll() {
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-notifications");
         options.addArguments("--disable-popup-blocking");
         options.setExperimentalOption("excludeSwitches", Arrays.asList("disable-popup-blocking"));
-        chromeDriver = new ChromeDriver(options);
-        chromeDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        chromeDriver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-        chromeDriver.manage().timeouts().setScriptTimeout(30, TimeUnit.SECONDS);
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().setScriptTimeout(30, TimeUnit.SECONDS);
 
-        chromeDriver.manage().window().maximize();
+        driver.manage().window().maximize();
 
-        chromeDriver.get("https://geekbrains.ru/events");
+        driver.get("https://geekbrains.ru/events");
 
-        wait15second = new WebDriverWait(chromeDriver, 15);
+        wait15second = new WebDriverWait(driver, 15);
     }
 
 
     @AfterEach
     public void BaseTestAfterAll() {
-        chromeDriver.quit();
+        driver.quit();
     }
 
 }
